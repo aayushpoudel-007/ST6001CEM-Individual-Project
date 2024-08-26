c07ec5191e86360aedb13093dcc872f6c5dba64c
Aayush Poudel <aayushp2906@gmail.com>
2024-08-26 21:55:15 +0545

    updated Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,9 +27,9 @@ Welcome to FlavorFiesta
 
 <Link
 to="/shop"
-className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
+className="bg-orange-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
 >
-Shop
+Recipes
 </Link>
 </div>
 
